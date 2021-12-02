9a24f0a09c9641526b2d2f8b493afdbe7cb2d796
T00212984 <eamon.healy@students.ittralee.ie>
2021-12-02 08:43:20 +0000

    project

--- src/Staff.java
+++ src/Staff.java
@@ -1,2 +1,60 @@
 public class Staff {
+    private String name;
+    private String address;
+    private int phone;
+    private String dob;
+    private String title;
+
+    public Staff(String name, String address, int phone, String dob, String title) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.dob = dob;
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Name: %s\nAddress: %s\nPhone: %s\nDOB: %s\nTitle", this.getName(), this.getAddress(), this.getPhone(), this.getDob(),this.getTitle());
+    }
 }
--- src/VetSys.java
+++ src/VetSys.java
@@ -9,6 +9,8 @@ public class VetSys extends JFrame implements ActionListener{
         JMenu ClientsMenu;
         JMenu PetsMenu;
         JLabel response;
+        JButton staffButton;
+        JButton buttonC;
 
         public VetSys() {
 
@@ -20,28 +22,34 @@ public class VetSys extends JFrame implements ActionListener{
             // invoke some user-defined methods to create two menus (and their menu items)
             // this is done just to make the constructor shorter but is good programming practice
 
-            createStaffMenu();
-            createClientsMenu();
-            createPetsMenu();
+            this.createStaffMenu();
+            this.createClientsMenu();
+            this.createPetsMenu();
 
 
 
             //and add them to the menubar
             JMenuBar menuBar = new JMenuBar();
-            setJMenuBar(menuBar);
+            this.setJMenuBar(menuBar);
             menuBar.setBackground(Color.gray);
-            menuBar.add(StaffMenu);
-            menuBar.add(ClientsMenu);
-            menuBar.add(PetsMenu);
+            menuBar.add(this.StaffMenu);
+            menuBar.add(this.ClientsMenu);
+            menuBar.add(this.PetsMenu);
+           //ImageIcon icon = new ImageIcon("");
+            //setIconImage(icon.getImage());
 
 
-            response = new JLabel("Veterinarian System" );
-            add(response);
+            this.response = new JLabel("Veterinarian System" );
+            //add(response);
 
             setSize(400,200);
             setDefaultCloseOperation(EXIT_ON_CLOSE);
-            setLocation(250,200);
+            setLocation(400,400);
             setVisible(true);
+
+            this.staffButton = new JButton("Add Staff");
+            this.staffButton.addActionListener(this);
+            this.buttonC.add(this.staffButton);
 
         }
 
@@ -50,53 +58,74 @@ public class VetSys extends JFrame implements ActionListener{
         }
 
         // when a menu-item is clicked, response starts here
-        public void actionPerformed(ActionEvent event) {
+        public void actionPerformed(ActionEvent e) {
+            int quit;
             String  menuName;
-            menuName = event.getActionCommand();
+            menuName = e.getActionCommand();
 
-            if (menuName.equals("Quit"))
-                System.exit(0);
-            else
-                response.setText("Menu Item '" + menuName + "' is selected.");
+            if(e.getActionCommand().equals("Register Staff")){
+                System.out.print("1");
+            }else if(e.getActionCommand().equals("View Staff")){
+                System.out.print("2");
+            }else if(e.getActionCommand().equals("Delete Staff")){
+                System.out.print("3");
+            }else if(e.getActionCommand().equals("Quit")) {
+                quit = JOptionPane.showConfirmDialog(null,"Are you sure you want to exit?","Exit",JOptionPane.YES_NO_OPTION);
+                if(quit==JOptionPane.YES_OPTION){
+                    JOptionPane.showMessageDialog(null,"Goodbye","Quit",JOptionPane.YES_OPTION);
+                    System.exit(0);
+                }
 
-        }
+            }else if(e.getActionCommand().equals("Add Client")){
+                System.out.print("4");
+            }else if(e.getActionCommand().equals("View Clients")) {
+                System.out.print("5");
+            }else if (e.getActionCommand().equals("Delete Clients")){
+                System.out.print("6");
+            }else if(e.getActionCommand().equals("Add Pet")){
+                System.out.print("7");
+            }else if(e.getActionCommand().equals("View Pets")){
+                System.out.print("8");
+            }else if(e.getActionCommand().equals("Delete Pets")) {
+                System.out.print("9");
+            }
+    }
+
+
 
         //Creates File menu and its menu items
         private void createStaffMenu( ) {
-            JMenuItem    item;
+            JMenuItem item;
 
-            StaffMenu = new JMenu("Staff");
+            this.StaffMenu = new JMenu("Staff");
 
             item = new JMenuItem("Register Staff");
             item.addActionListener( this );
-
-            StaffMenu.add( item );
+            this.StaffMenu.add( item );
 
             item = new JMenuItem("View Staff");
             item.addActionListener( this );
-            StaffMenu.add( item );
+            this.StaffMenu.add( item );
 
             item = new JMenuItem("Delete Staff");
             item.addActionListener( this );
-            StaffMenu.add( item );
+            this.StaffMenu.add( item );
 
-            StaffMenu.addSeparator(); //adds a horizontal separator line
+            this.StaffMenu.addSeparator(); //adds a horizontal separator line
 
             item = new JMenuItem("Quit");
             item.addActionListener( this );
-            StaffMenu.add( item );
+            this.StaffMenu.add( item );
         }
 
     private void createClientsMenu( ) {
-        JMenuItem    item;
+        JMenuItem item;
 
         ClientsMenu = new JMenu("Clients");
 
         item = new JMenuItem("Add Client");
         item.addActionListener( this );
-
         ClientsMenu.add( item );
-
 
         item = new JMenuItem("View Clients");
         item.addActionListener( this );
@@ -105,19 +134,16 @@ public class VetSys extends JFrame implements ActionListener{
         item = new JMenuItem("Delete Clients");
         item.addActionListener( this );
         ClientsMenu.add( item );
-
-
     }
 
 
     private void createPetsMenu() {
-        JMenuItem    item;
+        JMenuItem item;
 
         PetsMenu = new JMenu("Pets");
 
         item = new JMenuItem("Add Pet");
         item.addActionListener( this );
-
         PetsMenu.add( item );
 
         item = new JMenuItem("View Pets");
@@ -127,6 +153,11 @@ public class VetSys extends JFrame implements ActionListener{
         item = new JMenuItem("Delete Pets");
         item.addActionListener( this );
         PetsMenu.add( item );
+
+    }
+
+    public void registerStaff(){
+            JOptionPane.showMessageDialog(null,"hi","hi",JOptionPane.INFORMATION_MESSAGE);
 
     }
 
